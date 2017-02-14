bf7e28d16efbbda21d6a26e457a232d23cb2e4aa
Jonny Pickard <jonathandpickard@gmail.com>
2017-02-14 19:07:28 +0000

    Create User info react component library

--- app/index.js
+++ app/index.js
@@ -1,15 +1,55 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const HelloWorld = React.createClass({
+const USER_DATA = {
+  name: 'Jonny',
+  username: 'JonnyP',
+  avatarURL: 'https://avatars0.githubusercontent.com/u/16156445?v=3&s=460'
+};
+
+const ProfilePic = React.createClass({
   render: function() {
-    return (
-      <div> Hello World </div>
+    return <img src={this.props.imageURL} style={{height: 100, width: 100}}/>;
+  }
+});
+
+const ProfileLink = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+const ProfileName = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <strong>
+          {this.props.name}
+        </strong>
+      </div>
+    );
+  }
+});
+
+const Avatar = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <ProfilePic imageURL={this.props.user.avatarURL}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
